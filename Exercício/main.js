2f26bf482862a40b775fd3b1a489d6e07d160914
Edilson Bispo <edilson_pixot@hotmail.com>
2023-04-24 15:44:44 -0300

    Funções com number e string

---
+++ Exercício/main.js
@@ -0,0 +1,7 @@
+"use strict";
+function calculaValor(num1, num2) {
+    return num1 * num2;
+}
+function meuNome(nome) {
+    return "Olá" + nome;
+}
---
+++ Exercício/main.ts
@@ -0,0 +1,6 @@
+function calculaValor(num1: number, num2: number): number {
+    return num1 * num2;
+}
+function meuNome(nome: string): string {
+    return "Olá" + nome;
+}
